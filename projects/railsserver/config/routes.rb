15aed071b179563d5810e1a845088c04ffb2666e
your-name <atremblay@MB066-Adrien-Tremblay.local>
2014-07-18 15:28:56 +0800

    >:(

--- projects/railsserver/config/routes.rb
+++ projects/railsserver/config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'pages/info'
 
   root :to => redirect('/pages/landing')
+
   resources :landing
 
   # The priority is based upon order of creation: first created -> highest priority.
